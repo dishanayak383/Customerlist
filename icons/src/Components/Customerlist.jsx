90823d507c4f1bea699fdf81868fb3a6f0f36ad0
dishanayak383 <dishanayak383@gmail.com>
2025-02-24 14:36:45 +0530

    first commit

--- icons/src/Components/Customerlist.jsx
+++ icons/src/Components/Customerlist.jsx
@@ -7,16 +7,27 @@ export const Customerlist=()=>{
             <div className="sub-container">
                 <div className="customer">
                     <div className="list">
-                        <div className="dental"></div>
-                        <div className="list-customer"></div>
+                        <div className="dental">
+                        <img src="https://www.shutterstock.com/image-vector/tooth-3d-blue-dentist-icon-260nw-1681421581.jpg"/>
+                        </div>
+                        <div className="list-customer">
+                            <p>List Customer</p>
+                            <p> > </p>
+                            <p> </p>
+                        </div>
                     </div>
                     <div className="customer1">
                         <div className="first"></div>
                         <div className="second"></div>
                         <div className="third">
                             <div className="row"></div>
-                            <div className="row1"></div>
-                            <div className="row2"></div>                        
+                            <div className="row1">
+                                <div className="column"></div>
+                                <div className="column1"></div>
+                                <div className="column2"></div>
+                            </div>
+                           <div className="row2"></div>
+                           <div className="row3"></div>              
                         </div>
                     </div>
                 </div>
