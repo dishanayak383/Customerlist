bbe6c092fef78d269f5fd7eee4cd630465103373
dishanayak383 <dishanayak383@gmail.com>
2025-02-24 10:32:44 +0530

    first commit

---
+++ icons/src/Components/Customerlist.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "/.Customerlist.css";
+
+
+
+const Customerlist = () => {
+    return (
+        <div className=""
+    )
+}
---
+++ icons/src/Components/Customerlist.stories.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Customerlist from "./Customerlist.jsx";
+
+
+export default {
+    title:"Customerlist",
+    Compmonent:Customerlist,
+};
+
+export const Default = () => <Customerlist/>;
