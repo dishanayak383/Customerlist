4947a8a08a163dc3eee66504cd2ea1dd86fea8ac
dishanayak383 <dishanayak383@gmail.com>
2025-02-25 15:30:27 +0530

    first commit

--- icons/src/Components/Customerlist.jsx
+++ icons/src/Components/Customerlist.jsx
@@ -3,6 +3,14 @@ import './Customerlist.css'
 import { IoIosArrowForward } from "react-icons/io";
 import { CiSearch } from "react-icons/ci";
 import { IoNotificationsOutline } from "react-icons/io5";
+import { RxDashboard } from "react-icons/rx";
+import { GoPaste } from "react-icons/go";
+import { FiUser } from "react-icons/fi";
+import { CiCalendar } from "react-icons/ci";
+import { GrCube } from "react-icons/gr";
+import { BiDockTop } from "react-icons/bi";
+import { FiSearch } from "react-icons/fi";
+import { HiDotsHorizontal } from "react-icons/hi";
 
 export const Customerlist=()=>{
     return (
@@ -23,14 +31,89 @@ export const Customerlist=()=>{
                                 <p><CiSearch/></p>
                                 <p><IoNotificationsOutline/></p>
                             </div>
-                            <div className="list3"></div>
+                            <div className="list3">
+                                <img src="https://cdn2.iconfinder.com/data/icons/circle-avatars-1/128/050_girl_avatar_profile_woman_suit_student_officer-512.png"/>
+                                <a>Alexander Arnold</a>
+                                <a>as a Administrator</a>
+                            </div>
                         </div>
                     </div>
                     <div className="customer1">
                         <div className="first">
-                            <p></p>
+                            <p><RxDashboard /></p>
+                            <p><GoPaste /></p>
+                            <p><FiUser /></p>
+                            <p><CiCalendar /></p>
+                            <p><GrCube /></p>
+                            <p><BiDockTop /></p>
                         </div>
-                        <div className="second"></div>
+                        <div className="second">
+                            <div className="c1">
+                                <a>Patient Queue</a>
+                                <a><FiSearch /></a>
+                                <p>All</p>
+                                <p>Oldest</p>
+                                <p>Newest</p>
+                            </div>
+                            <div className="c2">
+                                <div className="im">
+                                    <img src="https://acade100.com/wp-content/uploads/2024/08/Image-26.png" width={"30px"} height={"30px"} />
+                                    <p><HiDotsHorizontal /></p>
+                                </div>
+                                <div className="nam">Bessie Cooper</div>
+                                <div className="reg">Register: 12 March 2023</div>
+                            </div>
+                            <div className="c3">
+                                 <div className="im">
+                                    <img src="https://acade100.com/wp-content/uploads/2024/08/Image-26.png" width={"30px"} height={"30px"} />
+                                    <p><HiDotsHorizontal /></p>
+                                </div>
+                                <div className="nam">Bessie Cooper</div>
+                                <div className="reg">Register: 12 March 2023</div>
+                            </div>
+                            <div className="c4">
+                                <div className="im">
+                                    <img src="https://acade100.com/wp-content/uploads/2024/08/Image-26.png" width={"30px"} height={"30px"} />
+                                    <p><HiDotsHorizontal /></p>
+                                </div>
+                                <div className="nam">Bessie Cooper</div>
+                                <div className="reg">Register: 12 March 2023</div>
+                            </div>
+                            <div className="c5">
+                            <div className="im">
+                                    <img src="https://acade100.com/wp-content/uploads/2024/08/Image-26.png" width={"30px"} height={"30px"} />
+                                    <p><HiDotsHorizontal /></p>
+                                </div>
+                                <div className="nam">Bessie Cooper</div>
+                                <div className="reg">Register: 12 March 2023</div>
+                            
+                            </div>
+                            <div className="c6">
+                            <div className="im">
+                                    <img src="https://acade100.com/wp-content/uploads/2024/08/Image-26.png" width={"30px"} height={"30px"} />
+                                    <p><HiDotsHorizontal /></p>
+                                </div>
+                                <div className="nam">Bessie Cooper</div>
+                                <div className="reg">Register: 12 March 2023</div>
+                            
+                            </div>
+                            <div className="c7">
+                            <div className="im">
+                                    <img src="https://acade100.com/wp-content/uploads/2024/08/Image-26.png" width={"30px"} height={"30px"} />
+                                    <p><HiDotsHorizontal /></p>
+                                </div>
+                                <div className="nam">Bessie Cooper</div>
+                                <div className="reg">Register: 12 March 2023</div>
+                            
+                            </div>
+                            <div className="c8">
+                            <div className="im">
+                                    <img src="https://acade100.com/wp-content/uploads/2024/08/Image-26.png" width={"30px"} height={"30px"} />
+                                    <p><HiDotsHorizontal /></p>
+                                </div>
+                                <div className="nam">Bessie Cooper</div>
+                            </div>
+                        </div>
                         <div className="third">
                             <div className="row"></div>
                             <div className="row1">
@@ -38,8 +121,10 @@ export const Customerlist=()=>{
                                 <div className="column1"></div>
                                 <div className="column2"></div>
                             </div>
-                           <div className="row2"></div>
-                           <div className="row3"></div>              
+                           <div className="row2">
+                               <div className="row3"></div>
+                           </div>
+                                          
                         </div>
                     </div>
                 </div>
